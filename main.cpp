bd9d9ea822974267b85c3c279642c3aaa0a8e68b
pouya-prh <pouya.bishe2@gmail.com>
2024-07-06 21:49:20 +0430

    I almost died to fix the tableWidget coordinate problem

--- main.cpp
+++ main.cpp
@@ -4,10 +4,10 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    // MainWindow w;
-    // w.show();
-    DialogSetGameBoard b;
-    b.show();
+    MainWindow w;
+    w.show();
+    // DialogSetGameBoard b;
+    // b.show();
 
 
     return a.exec();
